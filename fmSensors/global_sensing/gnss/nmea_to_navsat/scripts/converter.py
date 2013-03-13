cd10f3826e5aa6fefffb8e6a6ad7423d81108033
LeonBondeLarsen <lelar09@student.sdu.dk>
2013-03-13 17:38:55 +0100

    Tested nmea_to_navsat converter

--- fmSensors/global_sensing/gnss/nmea_to_navsat/scripts/converter.py
+++ fmSensors/global_sensing/gnss/nmea_to_navsat/scripts/converter.py
@@ -42,7 +42,7 @@ class converter():
         self.gpspub = rospy.Publisher('fix', NavSatFix)
         self.gpsVelPub = rospy.Publisher('vel',TwistStamped)
         self.gpstimePub = rospy.Publisher('time_reference', TimeReference)
-        self.nmea_topic = rospy.get_param("~nmea_topic",'/fmData/nmea_from_gps')
+        self.nmea_topic = rospy.get_param("~nmea_topic",'/fmLib/nmea_from_gps')
         self.status_sub = rospy.Subscriber(self.nmea_topic, nmea , self.onNmea)
         
         self.frame_id = rospy.get_param('~frame_id','gps')
@@ -90,13 +90,11 @@ class converter():
     def onNmea(self,msg):
         timeNow = rospy.get_rostime()
         
-        for i in msg.data:
-            i = i.strip(',')
         try:
             if self.useRMC:
                 #Check for satellite lock
                 if 'GSA' in msg.type:
-                    lockState = int(msg.data[2])
+                    lockState = int(msg.data[1])
                     #print 'lockState=',lockState
                     if lockState == 3:
                         self.GPSLock = True
@@ -108,8 +106,8 @@ class converter():
                         if 'RMC' in msg.type:
                             #print msg.data
                             self.gpsVel.header.stamp = timeNow
-                            self.gpsVel.twist.linear.x = float(msg.data[7])*0.514444444444*math.sin(float(msg.data[8]))
-                            self.gpsVel.twist.linear.y = float(msg.data[7])*0.514444444444*math.cos(float(msg.data[8]))
+                            self.gpsVel.twist.linear.x = float(msg.data[6])*0.514444444444*math.sin(float(msg.data[7]))
+                            self.gpsVel.twist.linear.y = float(msg.data[6])*0.514444444444*math.cos(float(msg.data[7]))
                             self.gpsVelPub.publish(self.gpsVel)
 
                             self.navData.status.status = NavSatStatus.STATUS_FIX
@@ -117,14 +115,14 @@ class converter():
                             self.navData.status.service = NavSatStatus.SERVICE_GPS
 
                             self.gpstime.header.stamp = self.gpsVel.header.stamp
-                            self.gpstime.time_ref = self.convertNMEATimeToROS(msg.data[1])
+                            self.gpstime.time_ref = self.convertNMEATimeToROS(msg.data[0])
 
-                            longitude = float(msg.data[5][0:3]) + float(msg.data[5][3:])/60
-                            if msg.data[6] == 'W':
+                            longitude = float(msg.data[4][0:3]) + float(msg.data[4][3:])/60
+                            if msg.data[5] == 'W':
                                 longitude = -longitude
 
-                            latitude = float(msg.data[3][0:2]) + float(msg.data[3][2:])/60
-                            if msg.data[4] == 'S':
+                            latitude = float(msg.data[2][0:2]) + float(msg.data[2][2:])/60
+                            if msg.data[3] == 'S':
                                 latitude = -latitude
 
                             #publish data
@@ -141,7 +139,7 @@ class converter():
                 #Use GGA
                 #No /vel output from just GGA
                 if 'GGA' in msg.type:
-                    gps_quality = int(msg.data[6])
+                    gps_quality = int(msg.data[5])
                     if gps_quality == 0:
                         self.navData.status.status = NavSatStatus.STATUS_NO_FIX
                     elif gps_quality == 1:
@@ -158,32 +156,32 @@ class converter():
 
                     self.navData.header.stamp = timeNow
 
-                    latitude = float(msg.data[2][0:2]) + float(msg.data[2][2:])/60
-                    if msg.data[3] == 'S':
+                    latitude = float(msg.data[1][0:2]) + float(msg.data[1][2:])/60
+                    if msg.data[2] == 'S':
                         latitude = -latitude
                     self.navData.latitude = latitude
 
-                    longitude = float(msg.data[4][0:3]) + float(msg.data[4][3:])/60
-                    if msg.data[5] == 'W':
+                    longitude = float(msg.data[3][0:3]) + float(msg.data[3][3:])/60
+                    if msg.data[4] == 'W':
                         longitude = -longitude
                     self.navData.longitude = longitude
 
-                    hdop = float(msg.data[8])
+                    hdop = float(msg.data[7])
                     self.navData.position_covariance[0] = hdop**2
                     self.navData.position_covariance[4] = hdop**2
                     self.navData.position_covariance[8] = (2*hdop)**2 #FIX ME
                     self.navData.position_covariance_type = NavSatFix.COVARIANCE_TYPE_APPROXIMATED
 
                     #Altitude is above ellipsoid, so adjust for mean-sea-level
-                    altitude = float(msg.data[9]) + float(msg.data[11])
+                    altitude = float(msg.data[8]) + float(msg.data[10])
                     self.navData.altitude = altitude
 
 
                     self.gpstime.header.stamp = timeNow
-                    self.gpstime.time_ref = convertNMEATimeToROS(msg.data[1])
+                    self.gpstime.time_ref = self.convertNMEATimeToROS(msg.data[0])
 
-                    self.gpspub.publish(navData)
-                    self.gpstimePub.publish(gpstime)
+                    self.gpspub.publish(self.navData)
+                    self.gpstimePub.publish(self.gpstime)
         except ValueError as e:
             rospy.logwarn("Value error, likely due to missing fields in the NMEA messages. Error was: %s" % e)
 
