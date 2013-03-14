d540c7561464cc8c1b643518f6d0d57d84bc92f4
LeonBondeLarsen <lelar09@student.sdu.dk>
2013-03-14 09:48:17 +0100

    Removed quantisation problem in wiimote interface and added test node

--- fmLib/hmi_support/wiimote_interface/setup.py
+++ fmLib/hmi_support/wiimote_interface/setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
+  scripts=['scripts/wii_interface_node.py'],
   packages=['wii_interface'],
   package_dir={'':'src'}
   )
