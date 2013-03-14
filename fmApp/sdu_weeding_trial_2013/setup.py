406125879da4ec1f6f2c198b06c0928742476fc4
Kjeld Jensen <kj@kjen.dk>
2013-03-14 11:13:37 +0100

    sdu_weeding_trial_2013: Working on navigating waypoints

---
+++ fmApp/sdu_weeding_trial_2013/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+  scripts=['src/show_map.py'],
+)
+
+setup(**d)
+
+
+
+
