f3470c19645468897b77be54d89e13a212721939
LeonBondeLarsen <lelar09@student.sdu.dk>
2013-05-16 11:11:03 +0200

    Improved behaviour around target

--- fmExecutors/platform/line_action_server/src/line_control/planner.py
+++ fmExecutors/platform/line_action_server/src/line_control/planner.py
@@ -315,29 +315,42 @@ class LinePlanner():
             self.z_ptr = 0  
         
         self.zone = (sum(self.zone_filter)/len(self.zone_filter))
-        if self.zone < (self.z1_value + ( (self.z2_value - self.z1_value) /2) ) :
+        
+        if self.zone < (self.z1_value + ( (self.z2_value - self.z1_value) /2) ):
             self.corrected = True
             self.rabbit_factor = self.z1_rabbit
             self.max_linear_velocity = self.z1_lin_vel
             self.max_angular_velocity = self.z1_ang_vel
-        elif self.zone < (self.z2_value + ( (self.z3_value - self.z2_value )/2) ) :
+            print("In zone 1")
+        elif self.zone < (self.z2_value + ( (self.z3_value - self.z2_value )/2) ):
             self.rabbit_factor = self.z2_rabbit
             self.max_linear_velocity = self.z2_lin_vel
             self.max_angular_velocity = self.z2_ang_vel
+            print("In zone 2")
         else :
             self.rabbit_factor = self.z3_rabbit
             self.max_linear_velocity = self.z3_lin_vel
             self.max_angular_velocity = self.z3_ang_vel
+            print("In zone 3")
         
-        if self.distance_to_goal < 3*self.max_distance_error :
-            self.max_linear_velocity *= self.distance_to_goal
-        elif self.distance_to_goal < self.z4_distance_to_target :
+#        if self.distance_to_goal < 3*self.max_distance_error :
+#            self.max_linear_velocity *= self.distance_to_goal
+        if self.distance_to_goal < self.z4_distance_to_target :
             self.rabbit_factor = 1
-            self.max_linear_velocity = self.max_linear_velocity / ( self.max_linear_velocity + (self.z4_distance_to_target - self.distance_to_goal)**2)
+            #self.max_linear_velocity = self.max_linear_velocity / ( self.max_linear_velocity + (self.z4_distance_to_target - self.distance_to_goal)**2)
+            self.max_linear_velocity = (self.max_linear_velocity/self.z4_distance_to_target) * self.distance_to_goal
             self.max_angular_velocity = self.z3_ang_vel 
+            print("Overwriting to zone 4.New speed: " + str(self.max_linear_velocity))
         elif not self.corrected:
-            self.max_linear_velocity *=  self.retarder
-            self.rabbit_factor = self.z3_rabbit
+            self.rabbit_factor = self.z3_rabbit * self.retarder
+            if math.fabs(self.angle_error) < self.z2_max_angle :
+                self.max_linear_velocity = self.z2_lin_vel
+            else :
+                self.max_linear_velocity = self.z3_lin_vel * self.retarder
+            self.max_angular_velocity = self.z3_ang_vel
+#            self.max_linear_velocity *=  self.retarder
+#            self.rabbit_factor = self.z3_rabbit
+            print("Uncorrected")
              
     def control_loop(self):
         """
