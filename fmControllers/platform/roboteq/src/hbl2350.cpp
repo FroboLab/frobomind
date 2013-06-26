badd28374f38f50982989678087ba96fdfb7a0e0
LeonBondeLarsen <lelar09@student.sdu.dk>
2013-06-26 16:18:34 +0200

    Testing pichi

--- fmControllers/platform/roboteq/src/hbl2350.cpp
+++ fmControllers/platform/roboteq/src/hbl2350.cpp
@@ -155,8 +155,8 @@ void hbl2350::initController(std::string config)
 	transmit(1,	"# C"); sleep(TIME_BETWEEN_COMMANDS);								// Clear buffer
 	transmit(1, "?CB"); sleep(TIME_BETWEEN_COMMANDS);								// Request absolute hall count
 	transmit(1,	"# 50" ); sleep(TIME_BETWEEN_COMMANDS);							    // Repeat buffer every 50 ms
-	transmit(1,	"^ALIM 1 70" ); sleep(TIME_BETWEEN_COMMANDS);
-	transmit(1,	"^ALIM 2 70" ); sleep(TIME_BETWEEN_COMMANDS);
+	transmit(1,	"^ALIM 1 700" ); sleep(TIME_BETWEEN_COMMANDS);
+	transmit(1,	"^ALIM 2 700" ); sleep(TIME_BETWEEN_COMMANDS);
 	transmit(1,	"^BLSTD 1 0" ); sleep(TIME_BETWEEN_COMMANDS);
 	transmit(1,	"^BLSTD 2 0" ); sleep(TIME_BETWEEN_COMMANDS);
 	sleep(2);
