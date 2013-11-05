7d3a53993af85631ec34f4c10966297c2fd0e788
Kjeld Jensen <kj@kjen.dk>
2013-11-05 12:30:45 +0100

    sdu_surveying: Getting ready for autonomous navigation.

--- fmApp/sdu_kjen_sandbox/sdu_surveying/setup.py
+++ fmApp/sdu_kjen_sandbox/sdu_surveying/setup.py
@@ -2,8 +2,11 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-  scripts=['scripts/cmd_vel_test.py'],
-)
+  scripts=['mission/simple_mission.py',
+  'navigation/waypoint_navigation_node.py',
+  'monitor/robot_track_map_node.py',
+  'scripts/show_pose_2d.py',
+  'scripts/cmd_vel_publish.py'],)
 
 setup(**d)
 
