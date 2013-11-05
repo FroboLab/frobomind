73c062ff21feab4ac707d6e4dac81a1da7727777
Kjeld Jensen <kj@kjen.dk>
2013-11-05 12:32:48 +0100

    sdu_surveying: Getting ready for navigation tests.

---
+++ fmApp/sdu_kjen_sandbox/sdu_surveying/bin/rsync_to_froboscout.sh
@@ -0,0 +1,2 @@
+rsync -avz --del ~/frobowork/src/* robot@192.168.10.60:~/frobowork/src
+
---
+++ fmApp/sdu_kjen_sandbox/sdu_surveying/bin/sim_wptnav.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# parameters
+WPTLIST='square_2m.txt'
+
+# change dir to the location of the shell script
+APPDIR=`dirname $(readlink -f $0)`
+cd $APPDIR
+
+# copy waypoint list to the ROS working directory
+cp ../waypoints/$WPTLIST ~/.ros/waypoints.txt
+
+# launch the ROS node
+roslaunch ../launch/sim_wptnav.launch
