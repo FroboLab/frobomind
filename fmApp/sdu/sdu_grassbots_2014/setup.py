ee79edf02d0c6b3dd3381ff77aa6fd124a473a3a
Kjeld Jensen <kj@kjen.dk>
2014-03-20 21:04:24 +0100

    sdu_grassbots_2014: Preparing for trials, not finished.

--- fmApp/sdu/sdu_grassbots_2014/setup.py
+++ fmApp/sdu/sdu_grassbots_2014/setup.py
@@ -3,11 +3,12 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
   scripts=[
-  'mission/simple_mission.py',
+  'mission/keyboard_mission.py',
+  'mission/wiimote_mission.py',
   'route/route_socketd.py',
   'navigation/area_coverage_casmo_node.py',
   'navigation/area_coverage_casmo_implement_node.py',
-  'monitor/robot_track_map_node.py'],
+  'monitor/show_navigation_status_node.py'],
 )
 
 setup(**d)
