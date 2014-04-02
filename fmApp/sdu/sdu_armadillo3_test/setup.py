2230d34646ec9005e78c0e799294f03671e83213
LeonBondeLarsen <lelar09@student.sdu.dk>
2014-04-02 12:10:52 +0200

    Testing Armadillo3

--- fmApp/sdu/sdu_armadillo3_test/setup.py
+++ fmApp/sdu/sdu_armadillo3_test/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-  scripts=['scripts/plot_pid_node.py'],
+  scripts=['scripts/plot_pid_node.py', 'mission/keyboard_mission_node.py'],
 )
 
 setup(**d)
