46a1448800c66c3c1fc2e714aef2c03cceb2c292
LeonBondeLarsen <lelar09@student.sdu.dk>
2014-04-06 21:17:48 +0200

    Translated library to C++

---
+++ fmLib/kinematics/differential_ifk_lib/src/DifferentialIfk.cpp
@@ -0,0 +1,31 @@
+/****************************************************************************
+ # FroboMind
+ # Licence and description in .hpp file
+ ****************************************************************************/
+#include "DifferentialIfk.hpp"
+
+DifferentialIfk::DifferentialIfk(double wheel_distance)
+{
+	_wheel_dist = wheel_distance;
+}
+
+DifferentialIfk::~DifferentialIfk()
+{
+
+}
+
+DifferentialIfk::twist_t DifferentialIfk::forward(double vel_left, double vel_right)
+{
+	DifferentialIfk::twist_t out;
+	out.linear = (vel_right + vel_left)/2.0;
+	out.angular = (vel_right - vel_left)/_wheel_dist;
+	return out;
+}
+
+DifferentialIfk::wheel_t DifferentialIfk::inverse(double vel_lin, double vel_ang)
+{
+	DifferentialIfk::wheel_t out;
+	out.left  = vel_lin - _wheel_dist*vel_ang;
+	out.right = vel_lin + _wheel_dist*vel_ang;
+	return out;
+}
