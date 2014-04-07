63b92878be53721228d6e45ae714150e0bc983c8
Kjeld Jensen <kj@kjen.dk>
2014-04-07 12:54:54 +0200

    sdu_hudem_2014: The script hudem_replay works now.

--- fmApp/sdu/sdu_surveying_2014/setup.py
+++ fmApp/sdu/sdu_surveying_2014/setup.py
@@ -6,10 +6,7 @@ d = generate_distutils_setup(
   'mission/wiimote_mission_node.py',
   'navigation/waypoint_navigation_node.py',
   'survey/survey_log_node.py',
-  'monitor/plot_lidar_node.py',
-  'monitor/plot_lidar_line_node.py',
   'monitor/show_imu_node.py',
-  'monitor/show_pose_node.py',
   'monitor/show_navigation_status_node.py'],)
 
 setup(**d)
