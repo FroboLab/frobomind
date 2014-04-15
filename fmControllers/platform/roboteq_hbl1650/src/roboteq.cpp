507ef2f371d9b4a33e6c9046219b3346a04edf79
LeonBondeLarsen <lelar09@student.sdu.dk>
2014-04-15 12:57:53 +0200

    Added exception handling in serial callback

--- fmControllers/platform/roboteq_hbl1650/src/roboteq.cpp
+++ fmControllers/platform/roboteq_hbl1650/src/roboteq.cpp
@@ -58,56 +58,67 @@ void RoboTeQ::serialCallback(const msgs::serial::ConstPtr& msg)
 	std::stringstream ss;
 	char dummy[25];
 
-	if(sscanf(msg->data.c_str(),"+%s",dummy))
-	{
-		ss << dummy << " ";
+	try {
+
+		if(sscanf(msg->data.c_str(),"+%s",dummy))
+		{
+			ss << dummy << " ";
+		}
+		else if(sscanf(msg->data.c_str(),"CB=%d",&cb1))
+		{
+			hall_feedback(msg->header.stamp, cb1);
+		}
+		else if(sscanf(msg->data.c_str(),"FF=%d",&ff))
+		{
+			ss << faultFlagsToString(ff) << " ";
+		}
+		else if(sscanf(msg->data.c_str(),"FS=%d",&fs))
+		{
+			ss << statusFlagsToString(fs) << " ";
+		}
+		else if(sscanf(msg->data.c_str(),"V=%d:%d:%d",&v1,&v2,&v3))
+		{
+			ss << "internal_voltage:" << v1/10.0 << "battery_voltage:" << v2/10.0 << " " << "5V_output_voltage:" << v3/1000.0 << " ";
+		}
+		else if(sscanf(msg->data.c_str(),"T=%d:%d",&t1,&t2))
+		{
+			ss << "temp_ic:" << t1 << " temp_ch1:" << t2 << " ";
+		}
+		else if(sscanf(msg->data.c_str(),"A=%d",&a1))
+		{
+			ss << "motor_amps_ch1:" << a1/10.0 << " ";
+		}
+		else if(sscanf(msg->data.c_str(),"BA=%d",&ba1))
+		{
+			ss << "battery_amps:" << ba1/10.0 << " ";
+		}
+		else if(sscanf(msg->data.c_str(),"FID=%s",dummy))
+		{
+			status.online = true;
+			status.initialised = false;
+			ROS_WARN("Found %s",msg->data.c_str());
+			initController("Standard");
+		}
+		else if(sscanf(msg->data.c_str(),"-%s",dummy))
+		{
+			ROS_WARN("RoboTeQ did not acknowledge the transmit");
+		}
+		else
+		{
+			ROS_WARN("RoboTeQ: %s",msg->data.c_str());
+		}
+		status_out.header.stamp = msg->header.stamp;
+		status_out.data = ss.str();
+		status_publisher.publish(status_out);
 	}
-	else if(sscanf(msg->data.c_str(),"CB=%d",&cb1))
+	catch (std::exception &e)
 	{
-		hall_feedback(msg->header.stamp, cb1);
+		ROS_INFO("RoboTeQ node threw an instance of %s", e.what());
 	}
-	else if(sscanf(msg->data.c_str(),"FF=%d",&ff))
+	catch (...)
 	{
-		ss << faultFlagsToString(ff) << " ";
+		ROS_INFO("RoboTeQ node threw unknown exception");
 	}
-	else if(sscanf(msg->data.c_str(),"FS=%d",&fs))
-	{
-		ss << statusFlagsToString(fs) << " ";
-	}
-	else if(sscanf(msg->data.c_str(),"V=%d:%d:%d",&v1,&v2,&v3))
-	{
-		ss << "internal_voltage:" << v1/10.0 << "battery_voltage:" << v2/10.0 << " " << "5V_output_voltage:" << v3/1000.0 << " ";
-	}
-	else if(sscanf(msg->data.c_str(),"T=%d:%d",&t1,&t2))
-	{
-		ss << "temp_ic:" << t1 << " temp_ch1:" << t2 << " ";
-	}
-	else if(sscanf(msg->data.c_str(),"A=%d",&a1))
-	{
-		ss << "motor_amps_ch1:" << a1/10.0 << " ";
-	}
-	else if(sscanf(msg->data.c_str(),"BA=%d",&ba1))
-	{
-		ss << "battery_amps:" << ba1/10.0 << " ";
-	}
-	else if(sscanf(msg->data.c_str(),"FID=%s",dummy))
-	{
-		status.online = true;
-		status.initialised = false;
-		ROS_WARN("Found %s",msg->data.c_str());
-		initController("Standard");
-	}
-	else if(sscanf(msg->data.c_str(),"-%s",dummy))
-	{
-		ROS_WARN("RoboTeQ did not acknowledge the transmit");
-	}
-	else
-	{
-		ROS_WARN("RoboTeQ: %s",msg->data.c_str());
-	}
-	status_out.header.stamp = msg->header.stamp;
-	status_out.data = ss.str();
-	status_publisher.publish(status_out);
 
 }
 
