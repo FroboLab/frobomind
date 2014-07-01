bd1867ebb9babae482e3d7ae9df25a5f8cb9f31d
Kjeld Jensen <kjeld@frobomind.org>
2014-07-01 16:28:40 +0200

    sdu_froboscout2_test: Still in development, now it is moving around :-)

--- fmApp/sdu/sdu_froboscout2_test/setup.py
+++ fmApp/sdu/sdu_froboscout2_test/setup.py
@@ -4,7 +4,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
   scripts=['mission/keyboard_mission_node.py',
   'mission/wiimote_mission_node.py',
-  'navigation/waypoint_navigation_node.py',
-  'monitor/show_navigation_status_node.py'],)
+  'navigation/waypoint_navigation_node.py'],)
 setup(**d)
 
