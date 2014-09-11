814c91a45579c2278802146c039569e124c5b6f1
Kjeld Jensen <kjeld@frobomind.org>
2014-09-11 22:40:28 +0200

    transverse_mercator: Fixed a package naming error in setup.py

--- fmLib/math/geographics/transverse_mercator/setup.py
+++ fmLib/math/geographics/transverse_mercator/setup.py
@@ -3,7 +3,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
   scripts=['scripts/kp2000_test.py','scripts/utm_test.py'],
-  packages=['transverse_mercator'],
+  packages=['transverse_mercator_py'],
   package_dir={'':'src'}
   )
 setup(**d)
