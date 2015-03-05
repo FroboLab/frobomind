6d14b6a661e64ef911e5fa7cb219d87442c4d12d
Kjeld Jensen <kj@kjen.dk>
2015-03-05 10:02:40 +0100

    pose_2d_estimator: Deleted and archived at github.com/SDU-Robotics/fmcomponents_archive

--- fmProcessors/localization_mapping/pose_2d_estimator/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-  scripts=['src/pose_2d_estimator_node.py', 'src/robot_track_map_node.py'],
-  packages=['pose_2d_estimator'],
-  package_dir={'':'src'}
-  )
-setup(**d)
--- fmProcessors/localization_mapping/pose_2d_estimator/simulation/run_simulation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-APPDIR=`dirname $(readlink -f $0)`
-cd $APPDIR
-
-export PYTHONPATH=$APPDIR/../src:$PYTHONPATH
-
-./simulate.py
-
