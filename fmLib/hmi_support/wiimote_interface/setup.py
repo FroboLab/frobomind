1620630927b78e4a235196c60df8d3a084153d82
Kjeld Jensen <kj@kjen.dk>
2015-10-04 14:03:03 +0200

    /fmLib/hmi_support/wiimote_interface: Old component migrated to the fmcomponent_archive repo.

--- fmLib/hmi_support/wiimote_interface/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-  scripts=['scripts/wii_interface_node.py'],
-  packages=['wii_interface'],
-  package_dir={'':'src'}
-  )
-setup(**d)
