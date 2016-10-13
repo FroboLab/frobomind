62e8a0a5dfd70786379c8ef99cdb42264ddd7757
kjeld <kjeld@pi>
2016-10-13 07:48:25 +0200

    /fmLib/math/geographics/transverse_mercator: Corrected an erroneous reference to the location of setup scripts.

--- fmLib/math/geographics/transverse_mercator/setup.py
+++ fmLib/math/geographics/transverse_mercator/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-  scripts=['scripts/utm_test.py', 'scripts/kp2000_test.py', 'scripts/dktm_test.py'],
+  scripts=['src/transverse_mercator_py/utm_test.py', 'src/transverse_mercator_py/kp2000_test.py', 'src/transverse_mercator_py/dktm_test.py'],
   packages=['transverse_mercator_py'],
   package_dir={'':'src'}
   )
